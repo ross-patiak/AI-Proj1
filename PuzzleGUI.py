9eb13a859caca15b7725babc769c924c4610f18c
Ross Patiak <ross.patiak@gmail.com>
2020-02-18 21:25:00 -0500

    second commit

--- PuzzleGUI.py
+++ PuzzleGUI.py
@@ -1,4 +1,31 @@
 from random import randint
+import matplotlib.pyplot as plt 
+
+def main():
+    board, size = build_GUI()
+    hill_climb(board, size)
+
+    
+def hill_climb(board, size):
+    x_vals = [(i+1) for i in range(50)]
+    y_vals = []
+    rand_celly = size-1
+    rand_cellx= size-1
+
+    while rand_cellx == size-1 and rand_celly == size-1:
+        rand_cellx = randint(0, size-1)
+        rand_celly = randint(0, size-1)
+    
+    print(rand_cellx)
+    print(rand_celly)
+
+
+    # for i in range(x_vals):
+    #     for j in range(i):
+    #         answer = i
+    #     y_vals.append()
+
+    
 
 def build_GUI():
     valid_sizes = [5, 7, 9, 11]
@@ -17,9 +44,9 @@ def build_GUI():
             else:
                 max_move = max(grid_size - (i+1), i, grid_size - (j+1), j)
                 grid[i][j] = randint(1, max_move)
-
-    print(grid)
+    
+    return (grid, grid_size)
 
 
 if __name__=='__main__':
-    build_GUI()
+    main()
